88d9a74fc8877544467b33eca3210f63ec358998
N-Talukder <nirzortalukder@gmail.com>
2020-09-30 19:54:09 +0600

    Wrote two interdependent functions to store inverse matrix of the input if calculated once and return the stored inverse matrix if it is encountered again.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## here are two functions which are interdependent. When a numeric matrix is
+## fed to the makeCacheMatrix(), then only the output can be sent as an input to
+## the cacheSolve() function which looks if the matrix's inverse was calculated 
+## once, and if yes, then it returns the cached value and if not, it calculates 
+## a new one and returns and also saves it.
 
-## Write a short comment describing this function
+## This makes a copy of the matrix fed to it and tries to remember the inverse
+## matrix of it, if ever calclualted. It also stores some handles in the returned
+## list output so that values can be stored to the makeCacheMatrix() output, if 
+## the inverse matrix is calculated from outside.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matrx = matrix()) {
+  
+  inv_matrx <- NULL
+  
+  set <- function(y){
+    
+    inv_matrx <<- NULL
+    matrx <<- y
+  }
+  
+  get <- function() matrx
+
+  setinv <- function(solve) inv_matrx <<-solve
+  
+  getinv <- function() inv_matrx
+  
+  list (set = set,
+        get = get,
+        getinv = getinv,
+        setinv = setinv)
 
 }
 
 
-## Write a short comment describing this function
+## the following function takes list output from the above makeCacheMatrix() 
+## funciton only. It checks if the inverse of the matrix was previously 
+## calculated. If yes, it retrieves the calculated inverse matrix from the 
+## list output of the makeCacheMatrix() and returns it with a printed message. 
+## If not, then it calclulates a new inverse matrix for that matrix and stores 
+## the new calculated inverse matrix to the makeCacheMatrix output usind some 
+## handles stored in the list output of that funciton makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachedM, ...) {
+        ## Returns a matrix that is the inverse of 'x'
+  
+        inv_matrx <- cachedM$getinv()
+        
+        if(!is.null(inv_matrx)) {
+          message("getting cached data")
+          return(inv_matrx)
+        }
+        
+        data <- cachedM$get()
+        inv_matrx <- solve(data)
+        cachedM$setinv(inv_matrx)
+        inv_matrx
 }
